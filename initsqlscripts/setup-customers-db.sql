c2bc2629687f6f646e6f7fc6424dd06e388074a9
Filipp Demchenko <filipp5632@gmail.com>
2024-08-04 18:37:00 +0300

    Remove redundant granting. Ownership transfer all prilliges

--- initsqlscripts/setup-customers-db.sql
+++ initsqlscripts/setup-customers-db.sql
@@ -2,5 +2,4 @@ CREATE ROLE customers_service WITH LOGIN PASSWORD 'pa55word';
 
 CREATE DATABASE customers_service;
 ALTER DATABASE customers_service OWNER TO customers_service;
-GRANT ALL ON DATABASE customers_service to customers_service;
 
